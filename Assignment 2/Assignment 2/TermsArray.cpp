c5e0be08423c45e9ef6608861342dd4ebb1dffaf
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-01-27 20:18:15 -0500

    updated

--- Assignment 2/Assignment 2/TermsArray.cpp
+++ Assignment 2/Assignment 2/TermsArray.cpp
@@ -1,1 +1,35 @@
 #include "TermsArray.h"
+
+TermsArray::TermsArray()
+{
+
+	this->sTermsArray[0] = "home";
+	this->sTermsArray[2] = "Accurate";
+	this->sTermsArray[3] = "trouble";
+	this->sTermsArray[4] = "random";
+	this->sTermsArray[5] = "tiger";
+	this->sTermsArray[6] = "ant";
+	this->sTermsArray[7] = "puzzle";
+	this->sTermsArray[8] = "apple";
+	this->sTermsArray[9] = "bluebarries";
+	this->sTermsArray[10] = "cookies";
+	this->sTermsArray[11] = "pineapple";
+
+
+}
+
+TermsArray::~TermsArray()
+{
+}
+
+string TermsArray::getSelectedTerm(int &iPos)
+{
+
+	return sTermsArray[iPos];
+
+}
+
+int TermsArray::getArraySize()
+{
+	return sizeof(sTermsArray);
+}
--- Assignment 2/Assignment 2/TermsArray.h
+++ Assignment 2/Assignment 2/TermsArray.h
@@ -1,7 +1,21 @@
 #pragma once
+#include <string>
+using namespace std;
 class TermsArray
 {
+private:
+
+	 string sTermsArray[12];
+
+public:
+
+	TermsArray();
+	~TermsArray();
+
 	
+	string getSelectedTerm(int&);
+	int getArraySize();
+
 
 };
 
